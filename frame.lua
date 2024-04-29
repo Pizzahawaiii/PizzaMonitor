f4db0bfdf9594ff875db35e7e803b51840f1695a
Pizzahawaii <ksorrr@gmail.com>
2024-04-29 22:42:34 +0200

    Init

---
+++ frame.lua
@@ -0,0 +1,63 @@
+local PWBM = PizzaWorldBuffs_Monitor
+local PWB = PizzaWorldBuffs
+
+PWBM.frame = CreateFrame('Frame', 'PizzaWorldBuffs_Monitor_Frame', UIParent)
+PWBM.frame:ClearAllPoints()
+PWBM.frame:SetPoint('TOP', 0, -50)
+PWBM.frame:SetFrameStrata('LOW')
+PWBM.frame:SetWidth(200)
+PWBM.frame:SetHeight(1)
+
+-- Mouse Drag
+PWBM.frame:SetMovable(true)
+PWBM.frame:EnableMouse(true)
+PWBM.frame:RegisterForDrag('leftButton')
+PWBM.frame:SetScript('OnDragStart', function ()
+  PWBM.frame:StartMoving()
+end)
+PWBM.frame:SetScript('OnDragStop', function ()
+  PWBM.frame:StopMovingOrSizing()
+end)
+
+-- Text
+PWBM.frame.text = PWBM.frame:CreateFontString('PizzaWorldBuffs_Monitor_Text', 'DIALOG', 'GameFontWhite')
+PWBM.frame.text:SetJustifyH('CENTER')
+PWBM.frame.text:SetPoint('CENTER', 0, 0)
+
+local function toTime(seconds)
+  local s = math.mod(seconds, 60)
+  local minutes = (seconds - s) / 60
+  local m = math.mod(minutes, 60)
+  local h = (minutes - m) / 60
+  return h, m, s
+end
+
+local function toTimeString(h, m, s)
+  if not h and not m and not s then return 'N/A' end
+  if h > 0 then
+    return string.format('%d:%d:%.2d', h, m, s)
+  else
+    return string.format('%d:%.2d', m, s)
+  end
+  -- return (h > 0 and h .. 'h ' or '') .. (m > 0 and m .. 'm ' or '') .. s .. 's'
+end
+
+PWBM.frame:SetScript('OnUpdate', function ()
+  -- Throttle this function so it doesn't run on every frame render
+  if (this.tick or 1) > GetTime() then return else this.tick = GetTime() + .1 end
+
+  PWBM.frame.content = 'PizzaWorldBuffs_Monitor\n'
+  
+  local playerCount = 0
+  local playerList = ''
+  for player, details in pairs(PWBM_seen) do
+    local lastSeenAgo = math.floor(GetTime() - details.lastSeen)
+    playerList = playerList .. '\n' .. player .. '|cff777777  ' .. toTimeString(toTime(lastSeenAgo)) .. '|r'
+    playerCount = playerCount + 1
+  end
+
+  PWBM.frame.content = PWBM.frame.content .. '\nUsers: ' .. playerCount .. '\n' .. playerList
+
+  PWBM.frame.text:SetText(PWBM.frame.content)
+  PWBM.frame:SetHeight(PWBM.frame.text:GetHeight() + 10)
+end)
---
+++ main.lua
@@ -0,0 +1,55 @@
+PizzaWorldBuffs_Monitor = CreateFrame('Frame', 'PizzaWorldBuffs_Monitor', UIParent)
+local PWBM = PizzaWorldBuffs_Monitor
+local PWB = PizzaWorldBuffs
+
+PWBM_seen = {}
+
+local timerStrs = {}
+
+PWBM:RegisterEvent('CHAT_MSG_CHANNEL')
+PWBM:SetScript('OnEvent', function ()
+  if event == 'CHAT_MSG_CHANNEL' then
+    local _, _, source = string.find(arg4, '(%d+)%.')
+    local channelName
+
+    if source then
+      _, channelName = GetChannelName(source)
+    end
+
+    -- Sender: arg2
+    if channelName == 'LFT' then
+      local addonName, version, msg = PWB.utils.strSplit(arg1, ':')
+      if addonName == PWB.abbrev then
+        local player = arg2
+
+        if player == 'Pizzahawaii' or player == 'Pizzamista' or player == 'Pizzabuffsa' or player == 'Pizzabuffsh' then
+          return
+        end
+
+        if PWBM_seen[player] then
+          PWBM_seen[player].lastSeen = GetTime()
+          PWBM_seen[player].version = version
+          PWBM_seen[player].messageCount = PWBM_seen[player].messageCount + 1
+        else
+          PWBM_seen[player] = {
+            firstSeen = GetTime(),
+            lastSeen = GetTime(),
+            version = version,
+            messageCount = 1,
+          }
+        end
+      end
+    end
+  end
+end)
+
+PWBM:SetScript('OnUpdate', function ()
+  -- Throttle this function so it doesn't run on every frame render
+  if (this.tick or 1) > GetTime() then return else this.tick = GetTime() + 1 end
+
+  for player, details in pairs(PWBM_seen) do
+    if GetTime() > details.lastSeen + 300 then
+      PWBM_seen[player] = nil
+    end
+  end
+end)
